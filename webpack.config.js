e061e11a462ff5026b3dc45634d4a3002ce92b88
Ahmedglez <ahmediglez@gmail.com>
2022-05-04 20:02:20 -0500

    empaqued

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+
+  mode: 'development',
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      Assets: path.resolve(__dirname, './src/assets'),
+      Components: path.resolve(__dirname, './src/components'),      
+      Pages: path.resolve(__dirname, './src/pages'),
+      Routes: path.resolve(__dirname, './src/routes'),
+      Styles: path.resolve(__dirname, './src/styles'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9015,
+  },
+};
